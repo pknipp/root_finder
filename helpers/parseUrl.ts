4a2049d064782be1a2cd8138fa97d20619794255
Peter Knipp <peteraknipp@gmail.com>
2024-11-05 11:45:50 -0500

    Handle erros from coefficients.

--- helpers/parseUrl.ts
+++ helpers/parseUrl.ts
@@ -6,39 +6,31 @@ export default (url: string): Result<ComplexNumber[]> => {
     // Remove spaces in order to prevent '%20' in address bar.
     url = url.replace(/\s+/g, '');
     let varName = "x";
-    let coefs: string[] = [];
+    let coefs: number[] = [];
     let char = url[0];
     if (char === '[') {
+        // Remove leading open-bracket.
         url = url.slice(1);
-        [url, char] = [url.slice(0,-1), url[-1]];
-        // if not str_temp[-1] == ']':
-                // return {"error": "You forgot the closing square bracket."}
-        // Convert string to array.
-        const coefs = url.split(",");
-        let nCoef = coefs.length;
-        const leadingCoef = Number(coefs[nCoef - 1]);
-        // isNaN(leadingCoef), return error
-        if (!leadingCoef) {
-            coefs.pop();
-            nCoef--;
+        char = url.slice(-1);
+        if (char !== ']') {
+            return {ok: false, error: "You forgot the closing square bracket."};
         }
-        // Tabulate these problems with user input.
-        let notANumber = 0;
-        for (const coef of coefs) {
-            if (!isNaN(Number(coef))) {
-                notANumber++
-            }
+        // Remove trailing close bracket.
+        url = url.slice(0, -1);
+        const stringCoefs = url.split(",");
+        const badCoefs = stringCoefs.filter(coef => isNaN(Number(coef)));
+        if (badCoefs.length) {
+            return {ok: false, error: `The following coefficient(s) is/are not parseable as a number: ${badCoefs}`};
         }
-        if (notANumber) {
-            // return {"error": str(not_a_number) + " of your coefficients is/are not a number."}
-        }
-        if (nCoef === 1) {
-            // return {"error": "Your polynomial seems to be constant, which'll lead to either zero roots or an infinite number thereof."}
+        const coefs = stringCoefs.map(coef => Number(coef));
+        while (coefs.length && !coefs[coefs.length - 1]) coefs.pop();
+        if (coefs.length <= 1) {
+            return {ok: false, error: "Your polynomial needs to be linear or higher-order."}
         }
     } else {
         return {ok: false, error: "This can presently handle a Url only if in array form."}
     }
-    const numericalCoefs = coefs.map(coef => Number(coef));
-    const roots = zroots(numericalCoefs, true);
+
+    const roots = zroots(coefs, true);
     return roots;
 }
