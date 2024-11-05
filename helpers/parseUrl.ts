708fa4b685318e4fce97249c29e62b353415b17c
Peter Knipp <peteraknipp@gmail.com>
2024-11-05 11:31:08 -0500

    Handle leading bracket absence error.

--- helpers/parseUrl.ts
+++ helpers/parseUrl.ts
@@ -6,35 +6,39 @@ export default (url: string): Result<ComplexNumber[]> => {
     // Remove spaces in order to prevent '%20' in address bar.
     url = url.replace(/\s+/g, '');
     let varName = "x";
-    let char;
-    [char, url] = [url[0], url.slice(1)];
-    [url, char] = [url.slice(0,-1), url[-1]];
-    // if not str_temp[-1] == ']':
-            // return {"error": "You forgot the closing square bracket."}
-    // Convert string to array.
-    const coefs = url.split(",");
-    let nCoef = coefs.length;
-    const leadingCoef = Number(coefs[nCoef - 1]);
-    // isNaN(leadingCoef), return error
-    if (!leadingCoef) {
-        coefs.pop();
-        nCoef--;
-    }
-    // Tabulate these problems with user input.
-    let notANumber = 0;
-    for (const coef of coefs) {
-        if (!isNaN(Number(coef))) {
-            notANumber++
+    let coefs: string[] = [];
+    let char = url[0];
+    if (char === '[') {
+        url = url.slice(1);
+        [url, char] = [url.slice(0,-1), url[-1]];
+        // if not str_temp[-1] == ']':
+                // return {"error": "You forgot the closing square bracket."}
+        // Convert string to array.
+        const coefs = url.split(",");
+        let nCoef = coefs.length;
+        const leadingCoef = Number(coefs[nCoef - 1]);
+        // isNaN(leadingCoef), return error
+        if (!leadingCoef) {
+            coefs.pop();
+            nCoef--;
         }
-    }
-    if (notANumber) {
-        // return {"error": str(not_a_number) + " of your coefficients is/are not a number."}
-    }
-    if (nCoef === 1) {
-        // return {"error": "Your polynomial seems to be constant, which'll lead to either zero roots or an infinite number thereof."}
+        // Tabulate these problems with user input.
+        let notANumber = 0;
+        for (const coef of coefs) {
+            if (!isNaN(Number(coef))) {
+                notANumber++
+            }
+        }
+        if (notANumber) {
+            // return {"error": str(not_a_number) + " of your coefficients is/are not a number."}
+        }
+        if (nCoef === 1) {
+            // return {"error": "Your polynomial seems to be constant, which'll lead to either zero roots or an infinite number thereof."}
+        }
+    } else {
+        return {ok: false, error: "This can presently handle a Url only if in array form."}
     }
     const numericalCoefs = coefs.map(coef => Number(coef));
-    // return numericalCoefs.join("-");
     const roots = zroots(numericalCoefs, true);
     return roots;
 }
